07c684185a3a6806e0559eee19b2c1f00a0f881d
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2025-04-01 13:51:31 -0300

    agrego librerias, y realizo la aritmetica de puntero

--- tp2_3.c
+++ tp2_3.c
@@ -1,14 +1,30 @@
+#include<stdio.h>   //Agrego biblioteca
 #define N 5
 #define M 7
-Int i,j;
-int mt[N][M];
-…
-for(i = 0;i<N; i++)
-{
-for(j = 0;j<M; j++)
-{
-mt[i][j]=1+rand()%100;
-printf(“%lf ”, mt[i][j]);
-}
-printf(“\n”);
+#include<stdlib.h>
+#include<time.h>
+
+int main() {
+
+    int i,j;
+    int mt[N][M];
+    int *puntero; //Declaro el puntero
+    puntero = mt; //Hago que el puntero apunte a la matriz
+
+    srand(time(NULL));
+
+    for(i = 0;i<N; i++)
+    {
+    for(j = 0;j<M; j++)
+    {
+    //Recordamos que MiMatriz[i][j]=*(MiMatriz+(i*numeroDeColumnas+j))
+    *(puntero+(i*M+j))=1+rand()%100; //Siendo M el numero de columnas
+    printf("%d ", *(puntero+(i*M+j)));
+    }
+    printf("\n");
+    }
+
+    getchar();
+    getchar();
+    return 0;
 }
