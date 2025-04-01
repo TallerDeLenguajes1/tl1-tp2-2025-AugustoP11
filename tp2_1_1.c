1b2e5fb1a78a9991171918190015b316f6e8f8a9
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2025-04-01 12:58:00 -0300

    agrego biblioteca, return, getchar y corrigo nombre de time

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -2,14 +2,19 @@
 #include<stdio.h>
 #include<time.h>
 #define N 20
+#include<stdlib.h>
 
 int main() {
 int i;
 double vt[N];
-srand(TIME(NULL));
+srand(time(NULL));
 
 for(i = 0;i<N; i++) {
     vt[i]=1+rand()%100;
-    printf("%f", vt[i]);
+    printf("%f, ", vt[i]);
 }
+
+getchar();
+getchar();
+return 0;
 }
