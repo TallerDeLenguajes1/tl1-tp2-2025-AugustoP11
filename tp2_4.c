2777677eba43bd05b86d44d6e5447251a01f4a4e
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2025-04-08 19:12:23 -0300

    Declaro el tipo de dato struct compu

---
+++ tp2_4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+//Estructuras
+struct compu{
+    int velocidad;
+    int anio;
+    int cantidad_nucleos;
+    char *tipo_cpu;
+};
+
+
+//Funcion principal
+int main() {
+
+
+
+    getchar();
+    return 0;
+}
