2f103a48b7e85f2db078d5371fd374410d1de5e1
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2025-04-08 20:11:08 -0300

    Agrego funcionalidad para generar aleatoriamente caracteristicas de 5 PCs

--- tp2_4.c
+++ tp2_4.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<string.h>
 
 //Estructuras
 struct compu{
@@ -13,8 +14,31 @@ struct compu{
 
 //Funcion principal
 int main() {
+    //Declaracion de variables
     char tipos[6][10]={"Intel","AMD","Celeron","Athlon","Core","Pentium"};
+    struct compu pcs[5];
+    int i=0;
+    int j;
 
+    //Seteo funcion random
+    srand(time(NULL));
+
+    //Genero aleatoriamente características de 5 PCs
+    for(i = 0 ; i < 5 ; i++)
+    {
+        //Velocidad aleatoria entre 1 y 3
+        pcs[i].velocidad = 1 + rand() % (3 - 1 + 1);
+
+        //Anio aleatorio entre 2015 y 2024
+        pcs[i].anio = 2015 + rand() % (2024 - 2015 + 1);
+
+        //Cantidad de nucleos aleatorios entre 1 y 8
+        pcs[i].cantidad_nucleos = 1 + rand() % (8 - 1 + 1);
+
+        //Hago que el campo tipo_cpu apunte a un string aleatorio del arreglo tipos
+        j = rand() % (5 + 1);
+        pcs[i].tipo_cpu = &tipos[j][10];
+    }
 
     getchar();
     return 0;
