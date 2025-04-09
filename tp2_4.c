da58813f84941aa8b898062e5c37fd92716f2a97
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2025-04-08 21:14:27 -0300

    Implemento la funcion listarPCs

--- tp2_4.c
+++ tp2_4.c
@@ -11,6 +11,9 @@ struct compu{
     char *tipo_cpu;
 };
 
+//Declaracion de funciones
+void listarPCs(struct compu pcs[], int cantidad);
+
 
 //Funcion principal
 int main() {
@@ -38,8 +41,26 @@ int main() {
         //Hago que el campo tipo_cpu apunte a un string aleatorio del arreglo tipos
         j = rand() % (5 + 1);
         pcs[i].tipo_cpu = &tipos[j][10];
-    }
+    }    
+
+    //Invoco la funcion para listar las PCs
+    listarPCs(pcs,5);
+
 
     getchar();
     return 0;
+}
+
+
+//Definicion de funciones
+void listarPCs(struct compu pcs[], int cantidad){
+    int i;
+    for (i = 0 ; i < cantidad ; i++)
+    {
+        printf("\nCARACTERISTICAS DE PC %d\n",(i+1));
+        printf("Velocidad: %dGHz\n", pcs[i].velocidad);
+        printf("Anio: %d\n", pcs[i].anio);
+        printf("Cantidad de nucleos: %d\n", pcs[i].cantidad_nucleos);
+        printf("Tipo de CPU: %s\n", pcs[i].tipo_cpu);
+    }
 }
